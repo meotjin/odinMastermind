6c3dcc3bab1594775ab1426d5609cfeaea0e2319
Kurosh Hesamshariati <kuroshhesamshariati@gmail.com>
2023-01-08 09:35:26 +0330

    create Board class

--- board.rb
+++ board.rb
@@ -1,2 +1,30 @@
 class Board
+  def initialize(answer)
+    @answer = answer
+    @geusses = 0
+  end
+
+  def geuss(com)
+    @geusses += 1
+    return nil if @geusses > 12
+
+    array = com.split('').map!(&:to_i)
+    [check_win(array),
+     check_corrects(array),
+     check_color(array)]
+  end
+
+  private
+
+  def check_win(array)
+    array == @answer
+  end
+
+  def check_corrects(array)
+    @answer.select.with_index { |item, index| item == array[index] }.count
+  end
+
+  def check_color(array)
+    array.select { |item| @answer.include?(item) }.count - check_corrects(array)
+  end
 end
